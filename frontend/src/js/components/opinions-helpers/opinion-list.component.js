bb3acc6e29a74e632f64777392d6c3ddf1993251
Alex Zarazua <alexzarazua88@gmail.com>
2020-11-04 17:39:38 +0100

    Solved error in Opinion preview and img readme

--- frontend/src/js/components/opinions-helpers/opinion-list.component.js
+++ frontend/src/js/components/opinions-helpers/opinion-list.component.js
@@ -2,12 +2,13 @@ class OpinionsListCtrl {
     constructor(Opinions) {
         "ngInject";
         console.log('---- OPINIONS LIST ----');
+
+        this._Opinions = Opinions;
   
         this.$onInit = () => {
-            this._Opinions = Opinions;
-
             this._Opinions.query().then(res => {
                 console.log('OPINIONESSS------->', res.opinions);
+                console.log(res.opinions)
                 this.opinions = res.opinions;
             })
         }
